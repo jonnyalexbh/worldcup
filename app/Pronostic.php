ab7be55b87915b2c4b3ba5aff9c6091b62c448c0
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-06-11 14:55:43 -0500

    customizing user registration

--- app/Pronostic.php
+++ app/Pronostic.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pronostic extends Model
 {
-    //
+  protected $fillable = ['user_id', 'match_id', 'team_h', 'team_a', 'score_h', 'score_a', 'pen_h', 'pen_a'];
 }
--- app/User.php
+++ app/User.php
@@ -7,23 +7,37 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = [
+    'name', 'email', 'password', 'username', 'status'
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+  * The attributes that should be hidden for arrays.
+  *
+  * @var array
+  */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+  /*
+  * create initial pronostics for user
+  *
+  */
+  public function create_pronostics() {
+    for($i =1; $i<65; $i++) {
+      $pronostic = Pronostic::firstOrCreate(
+        [
+          'user_id'=>$this->id,
+          'match_id'=>$i
+        ]
+      );
+    }
+  }
 }
